43903d00a27eaaaac49081832b3a5ad4287e34a2
Aleksei Serdiuk <aleksey.serduyk@gmail.com>
2022-09-07 14:35:46 +0600

    Innitial commit

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Алексей Сердюк on 07.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
